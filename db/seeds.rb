4e504a1563928a28a07b583c741c6b64a75d6153
Angie Duhard <angie.duhard@gmail.com>
2023-07-18 12:37:13 +0200

    added user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts " creating 2 users"
+User.create!(last_name: "Jones", first_name: "Martin", email: "martinjones@gmail.com", password: "jdiehoej44")
+User.create!(last_name: "Doe", first_name: "Jane", email: "janedoe@gmail.com", password: "jdi87ej44")
+puts "users created"
